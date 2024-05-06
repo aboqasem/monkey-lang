25e67cff9ac3dcc82c323b1d6583de035a0ef57e
Mohammad Al Zouabi <mb.alzouabi@gmail.com>
2024-05-06 09:21:41 +0800

    feat: initial parsing with `parseLetStatement`

--- parser/parser.go
+++ parser/parser.go
@@ -25,6 +25,62 @@ func (p *Parser) next() {
 	p.nextToken = p.l.NextToken()
 }
 
+func (p *Parser) parseStatement() ast.Statement {
+	switch p.currToken.Type {
+	case token.LET:
+		return p.parseLetStatement()
+	default:
+		return nil
+	}
+}
+
+func (p *Parser) parseLetStatement() (stmt *ast.LetStatement) {
+	stmt = &ast.LetStatement{Token: p.currToken}
+
+	if !p.expectNext(token.IDENT) {
+		return nil
+	}
+
+	stmt.Name = &ast.Identifier{Token: p.currToken, Value: p.currToken.Literal}
+
+	if !p.expectNext(token.ASSIGN) {
+		return nil
+	}
+
+	// TODO: We're skipping the expressions until we encounter a semicolon
+	for !p.currTokenIs(token.SEMICOLON) {
+		p.next()
+	}
+
+	return
+}
+
+func (p *Parser) currTokenIs(t token.TokenType) bool {
+	return p.currToken.Type == t
+}
+
+func (p *Parser) nextTokenIs(t token.TokenType) bool {
+	return p.nextToken.Type == t
+}
+
+func (p *Parser) expectNext(t token.TokenType) bool {
+	if !p.nextTokenIs(t) {
+		return false
+	}
+	p.next()
+	return true
+}
+
 func (p *Parser) Parse() (prog *ast.Program) {
+	prog = &ast.Program{}
+	prog.Statements = []ast.Statement{}
+
+	for p.currToken.Type != token.EOF {
+		stmt := p.parseStatement()
+		if stmt != nil {
+			prog.Statements = append(prog.Statements, stmt)
+		}
+		p.next()
+	}
 	return
 }
