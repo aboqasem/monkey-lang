62ed3ce68c943ee78012dd49daca39ae2bed2193
Mohammad Al Zouabi <mb.alzouabi@gmail.com>
2024-05-14 21:11:11 +0800

    feat(ast): add & implement `Node.String()`

--- ast/ast.go
+++ ast/ast.go
@@ -1,9 +1,13 @@
 package ast
 
-import "monkey-lang/token"
+import (
+	"bytes"
+	"monkey-lang/token"
+)
 
 type Node interface {
 	TokenLiteral() string
+	String() string
 }
 
 type Statement interface {
@@ -28,6 +32,15 @@ func (p *Program) TokenLiteral() (lit string) {
 	return
 }
 
+func (p *Program) String() string {
+	out := bytes.Buffer{}
+	for _, stmt := range p.Statements {
+		out.WriteString(stmt.String())
+		out.WriteRune('\n')
+	}
+	return out.String()
+}
+
 type LetStatement struct {
 	Token token.Token
 	Name  *Identifier
@@ -36,6 +49,20 @@ type LetStatement struct {
 
 func (s *LetStatement) statementNode()       {}
 func (s *LetStatement) TokenLiteral() string { return s.Token.Literal }
+func (s *LetStatement) String() string {
+	out := bytes.Buffer{}
+	out.WriteString(s.TokenLiteral())
+	out.WriteRune(' ')
+	out.WriteString(s.Name.String())
+	out.WriteString(" = ")
+    // TODO: should not be nil, remove when fully implemented
+	if v := s.Value; v != nil {
+		out.WriteString(v.String())
+	}
+	out.WriteRune(';')
+
+	return out.String()
+}
 
 type ReturnStatement struct {
 	Token token.Token
@@ -44,6 +71,18 @@ type ReturnStatement struct {
 
 func (s *ReturnStatement) statementNode()       {}
 func (s *ReturnStatement) TokenLiteral() string { return s.Token.Literal }
+func (s *ReturnStatement) String() string {
+	out := bytes.Buffer{}
+	out.WriteString(s.TokenLiteral())
+	out.WriteRune(' ')
+    // TODO: should not be nil, remove when fully implemented
+	if v := s.Value; v != nil {
+		out.WriteString(v.String())
+	}
+	out.WriteRune(';')
+
+	return out.String()
+}
 
 type Identifier struct {
 	Token token.Token
@@ -52,3 +91,4 @@ type Identifier struct {
 
 func (i *Identifier) expressionNode()      {}
 func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
+func (i *Identifier) String() string       { return i.Value }
